987e8fae93594a285a762395303c06139b5264a5
hennndev <hendraputra230897@gmail.com>
2024-11-02 21:43:41 +0700

    bug-fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,18 +17,9 @@ const config: Config = {
         "visimisi-image": "url('/images/visimisi-image.png')"
       },
       boxShadow: {
-        "dropdown": "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
-        "card": "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;"
-      },
-      animation: {
-        'infinite-scroll': 'infinite-scroll 25s linear infinite',
-      },
-      keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-      }       
+        "dropdown": "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+        "card": "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
+      },   
     },
   },
   plugins: [
